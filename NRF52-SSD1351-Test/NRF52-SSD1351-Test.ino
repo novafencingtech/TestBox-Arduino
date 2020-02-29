bef45e7d964cdda9ae9c5846c96b44a5031ae413
gsallen103 <48398825+gsallen103@users.noreply.github.com>
2020-02-28 20:50:30 -0500

    Add display test code & binaries
    Added the most recent master binary build.

---
+++ NRF52-SSD1351-Test/NRF52-SSD1351-Test.ino
@@ -0,0 +1,46 @@
+// Screen dimensions
+#define SCREEN_WIDTH  128
+#define SCREEN_HEIGHT 128 // Change this to 96 for 1.27" OLED.
+
+// You can use any (4 or) 5 pins
+#define SCLK_PIN 12
+#define MOSI_PIN 13
+#define DC_PIN   20
+#define CS_PIN   31
+//#define RST_PIN  20
+
+
+// Color definitions
+#define  BLACK           0x0000
+#define BLUE            0x001F
+#define RED             0xF800
+#define GREEN           0x07E0
+#define CYAN            0x07FF
+#define MAGENTA         0xF81F
+#define YELLOW          0xFFE0  
+#define WHITE           0xFFFF
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1351.h>
+#include <SPI.h>
+
+// Option 1: use any pins but a little slower
+Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, CS_PIN, DC_PIN, MOSI_PIN, SCLK_PIN);  
+
+// Option 2: must use the hardware SPI pins
+// (for UNO thats sclk = 13 and sid = 11) and pin 10 must be
+// an output. This is much faster - also required if you want
+// to use the microSD card (see the image drawing example)
+//Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, CS_PIN, DC_PIN);
+
+void setup() {
+  tft.begin();
+  tft.fillRect(0, 0, 128, 128, BLACK);
+  tft.drawLine(0,0,127, 127, WHITE);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
