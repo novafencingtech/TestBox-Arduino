0a11c77f1e8c296b178f94f05a899de72ab4a653
gsallen103 <48398825+gsallen103@users.noreply.github.com>
2021-07-25 09:37:11 -0700

    Moving custom graphics to it's own branch
    Moved custom graphics to a dedicated branch.

--- Feather_TestBox_v0/Feather_TestBox_v0.ino
+++ Feather_TestBox_v0/Feather_TestBox_v0.ino
@@ -43,8 +43,9 @@ static const char BUILD_DATE[16] = "2021-06-12";
 
 
 #ifdef DISPLAY_SPLASH_IMAGE
-#include "splashScreenImage.c"
+//#include "splashScreenImage.c"
 //#include "NOVA_logo_basic.c"
+#include "NOVA_logo.c"
 //#include "Tim'sBox.c"
 #else 
 #define DISPLAY_SPLASH_IMAGE 0
