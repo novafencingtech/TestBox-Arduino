ecfdd4f473f79193f81728a02187aee923b90a1d
gsallen103 <48398825+gsallen103@users.noreply.github.com>
2023-10-23 21:33:05 -0700

    Update Feather_TestBox_v0.ino
    Updated default to the Rev 2 boards with FastLED enabled.

--- Feather_TestBox_v0/Feather_TestBox_v0.ino
+++ Feather_TestBox_v0/Feather_TestBox_v0.ino
@@ -4,7 +4,7 @@
 #define ARM_MATH_CM4 //Required for arm_math library
 #define __FPU_PRESENT 1
 
-#define TTArmBoardRev 1 
+#define TTArmBoardRev 2 
 
 // Used for defining board revision specific changes and features
 #if (TTArmBoardRev>=2)
@@ -37,7 +37,7 @@ using namespace Adafruit_LittleFS_Namespace;
 #include "oledDisplaySettings.h"
 
 #define DISPLAY_SPLASH_IMAGE 1
-#define FAST_LED_ACTIVE 0
+#define FAST_LED_ACTIVE 1
 
 #if FAST_LED_ACTIVE 
 #define FASTLED_INTERRUPT_RETRY_COUNT 3
